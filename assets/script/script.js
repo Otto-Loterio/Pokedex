d2c63198ae2b3f28e1a705b040a695e3f7e7c054
Otto Lotério <ottopequenostal@gmail.com>
2025-07-23 05:50:48 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,33 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 5;
+let offset = 0;
+const maxRecords = 151
+
+function loadPokemonItens(offset, limit) {
+    pokeAPI.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name"> ${pokemon.name} </span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+                    </ol>
+
+                    <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+`).join('')
+        pokemonList.innerHTML += newHTML
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
